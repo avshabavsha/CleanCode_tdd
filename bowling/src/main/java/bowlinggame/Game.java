744472969b50b4f5870cf889b0cdc7f68b9a7a57
avshalom <avshalom.sch@gmail.com>
2018-03-03 19:18:51 +0200

    ignore again - more refactor is needed_2

--- bowling/src/main/java/bowlinggame/Game.java
+++ bowling/src/main/java/bowlinggame/Game.java
@@ -14,7 +14,6 @@ public class Game {
 
     public int score() {
         for (int i = 0; i < rolls.length; i++) {
-            if(rolls[i] + rolls[i+1] == 10)
             score += rolls[i];
         }
         return score;
