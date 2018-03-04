82c890dfd5bbb387b115e1cced552bb447f3bea2
avshaloms <avshalom.schwartz@nice.com>
2018-03-04 15:26:37 +0200

    add Name inverter base project

---
+++ nameinverter/src/test/java/nameInverter/NameInverterTest.java
@@ -0,0 +1,15 @@
+package nameInverter;
+
+import org.junit.Test;
+
+/**
+ * Created by avshaloms on 04/03/2018.
+ */
+public class NameInverterTest {
+
+    @Test
+    public void nothing() throws Exception {
+
+
+    }
+}
