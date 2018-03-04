c8bf563c673ae52f8427520880dc380149e75587
avshaloms <avshalom.schwartz@nice.com>
2018-03-04 15:30:13 +0200

    first test

--- nameinverter/src/test/java/nameInverter/NameInverterTest.java
+++ nameinverter/src/test/java/nameInverter/NameInverterTest.java
@@ -2,14 +2,20 @@ package nameInverter;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by avshaloms on 04/03/2018.
  */
 public class NameInverterTest {
 
     @Test
-    public void nothing() throws Exception {
-
-
+    public void givenNull_ReturnsEmptyString() throws Exception {
+        assertEquals("", invertName(null));
     }
+
+    private String invertName(String name) {
+        return "";
+    }
+
 }
