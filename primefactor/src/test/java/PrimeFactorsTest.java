41e9b0f43967219531780ae28e47ad7f9ae602d3
avshaloms <avshalom.schwartz@nice.com>
2018-03-07 07:23:41 +0200

    PrimeFactors - init

---
+++ primefactor/src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by avshaloms on 07/03/2018.
+ */
+public class PrimeFactorsTest {
+    @Test
+    public void nothing() throws Exception {
+        
+
+    }
+}
