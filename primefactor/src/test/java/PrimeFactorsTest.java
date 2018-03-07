38e0ce549abfbb7489286fe499eb0d9de1e15200
avshaloms <avshalom.schwartz@nice.com>
2018-03-07 07:28:18 +0200

    PrimeFactors - add failed test

--- primefactor/src/test/java/PrimeFactorsTest.java
+++ primefactor/src/test/java/PrimeFactorsTest.java
@@ -1,12 +1,24 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by avshaloms on 07/03/2018.
  */
 public class PrimeFactorsTest {
     @Test
-    public void nothing() throws Exception {
-        
+    public void canFactorIntoPrimes() throws Exception {
+        Assert.assertEquals(list(), of(1));
 
+    }
+
+    private List<Integer> of(int n) {
+        return null;
+    }
+
+    private List<Integer> list() {
+        return Arrays.asList();
     }
 }
